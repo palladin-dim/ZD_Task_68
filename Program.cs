c2237e8f0bb833acf39a9bd94e3439666c73effa
palladin-dim <103898528+palladin-dim@users.noreply.github.com>
2022-10-03 00:43:18 +0600

    Task_68

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9 */
+
+Console.Clear();
+int m = InputNumbers("Введите неотрицательное число m: ");
+int n = InputNumbers("Введите неотрицательное число n: ");
+
+int functionAkkerman = Akkerman(m, n);
+Console.Write($"A(m,n) = {functionAkkerman} ");
+
+int Akkerman(int m, int n)
+{
+    if(m == 0) return n + 1;
+    else if(m > 0 && n == 0)
+    {
+        return Akkerman(m-1, 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+        return Akkerman(m - 1, Akkerman(m, n -1));
+    }
+    return Akkerman(m, n);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
